805b2b4b29627d58862ce73dcb4186b13a6f6bf0
bigbun <1124371650@qq.com>
2015-01-23 10:03:16 +0800

    hh

--- ZDsdkDemo/src/com/demo/WelComeActivity.java
+++ ZDsdkDemo/src/com/demo/WelComeActivity.java
@@ -1,9 +1,12 @@
 package com.demo;
 
+import java.util.zip.Inflater;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
