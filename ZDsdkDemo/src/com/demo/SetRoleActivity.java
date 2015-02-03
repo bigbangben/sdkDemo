13af9e46d985001713ad56dd0b99c7978fdd6a06
bigbun <1124371650@qq.com>
2015-02-03 14:38:10 +0800

    aaa

--- ZDsdkDemo/src/com/demo/SetRoleActivity.java
+++ ZDsdkDemo/src/com/demo/SetRoleActivity.java
@@ -9,6 +9,7 @@ import android.view.Window;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.demo.nearme.gamecenter.R;
 import com.zhidian.issueSDK.ICallback;
 import com.zhidian.issueSDK.ZDSDK;
 import com.zhidian.issueSDK.model.GameInfo;
--- ZDsdkDemo/src/com/demo/WelComeActivity.java
+++ ZDsdkDemo/src/com/demo/WelComeActivity.java
@@ -10,6 +10,7 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.demo.nearme.gamecenter.R;
 import com.zhidian.issueSDK.ICallback;
 import com.zhidian.issueSDK.ZDSDK;
 import com.zhidian.issueSDK.model.InitInfo;
